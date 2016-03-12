2eed4ec54318905d0975b3a03b4a885273079a53
hefju <hefju@qq.com>
2016-03-12 11:24:33 +0800

    测试简单工厂返回数据

---
+++ JuBuilder2/IDatabase.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuBuilder2
+{
+
+    interface IDatabase
+    {
+        bool Connect(string ConnectString);
+
+        bool Open();
+
+        bool Command(string SQL);
+
+        void Close();
+    }
+
+    public class SqlServer : IDatabase
+    {
+        SqlConnection conn;
+        SqlCommand command;
+
+        public bool Connect(string ConnectString)
+        {
+            try
+            {
+                conn = new SqlConnection(ConnectString);
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+
+        public bool Open()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        public bool Command(string SQL)
+        {
+            try
+            {
+                command = new SqlCommand(SQL, conn);
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+        public void Close()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+
+    public class Oracle : IDatabase
+    {
+        public Oracle()
+        {
+        }
+        public bool Connect(string ConnectString)
+        {
+            return true;
+        }
+        public bool Open()
+        {
+            return true;
+        }
+        public bool Command(string SQL)
+        {
+            return true;
+        }
+
+        public void Close()
+        {
+        }
+    }
+
+    public class Factory
+    {
+        public static IDatabase SelectDatabase(string DatabaseType)
+        {
+            switch (DatabaseType)
+            {
+                case "SqlServer":
+                    return new SqlServer();
+                case "Oracle":
+                    return new Oracle();
+                default:
+                    return new SqlServer();
+            }
+        }
+    }
+}
